8b3128692708c449f8bcdbc9b1eee31dab0906a4
Luana <lualuna182@gmail.com>
2021-02-26 21:30:33 -0300

    Ajustes finais do desafio

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -1,133 +1,58 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
 
 
-    console.log("Bem vindo ao jogo BlackJack!")
+         console.log("Bem vindo ao jogo BlackJack!")
 
-    let user1 = comprarCarta();
-    let user2 = comprarCarta();
+    // input usuário
+         let novaRodada = confirm("Quer iniciar uma nova rodada?")
+    // comprar carta
+         let novaCompra = comprarCarta();
+   // container para armazenar cartas jogadas pelo usuário
+         let cartas = [];
+    // pontos usuário
+         let points = 0;
+   // container para armazenar cartas jogadas pela máquina
+         let cartasPc = []
+    // pontos máquina
+         let pointsPc = 0;
 
-    let pc1 = comprarCarta();
-    let pc2 = comprarCarta();
+   
 
-    let res1 = user1.valor + user2.valor;
-    let res2 = pc1.valor + pc2.valor;
+   // verifica se o usuário clica ok para iniciar o jogo
+      if (novaRodada) {
+         while (novaRodada) {
+            // compra do usuário
+            novaCompra = comprarCarta();
+            cartas.push(novaCompra.texto);
+            points += novaCompra.valor
+            // compra da máquina
+            novaCompra = comprarCarta();
+            cartasPc.push(novaCompra);
+            pointsPc += novaCompra.valor
+               // verifica se as duas cartas iniciais são Ases, se diferentes, jogo inicia
+               if (cartas[0] !== cartasPc[0]) {
+               //revelação das cartas
+               console.log("Suas cartas são ---->  " + cartas + ". || A carta revelada do computador é " + novaCompra.texto)
+                // verifica se as duas cartas iniciais são Ases, se iguais, novo sorteio é feito
+               } else if (cartas[0] === cartasPc[0]) {
+                  console.error("Ops! As duas primeiras cartas são Ases.")
+                  novaRodada = confirm("Novo sorteio?")
+               } 
 
-    let novaCompra = comprarCarta();
+                  // finaliza o jogo quando algum dos jogadores atinge 21 pontos
+                  if (points < 21 || pointsPc < 21) {
+                     novaRodada = confirm("Deseja comprar mais uma carta?")
+                     } else if (pointsPc > points) {
+                        console.error("Você perdeu :(");
+                        console.log("O computador fez " + pointsPc + " pontos.")
+                        break
+                     } else if (points > pointsPc) {
+                        console.warn("Você ganhou!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                        console.log("Parabéns, fez " + points + " pontos.")
+                        break
+                     }
 
-    let novaRodada = confirm("Quer iniciar uma nova rodada?")
-
-    let points = 0;
-    let pointsPc = 0;
-    let cartas = [];
-    let cartaspc;
-
-
-    if(!novaRodada) {
-      console.log("O jogo acabou :(")
-   } else if (novaRodada) {
-     while (novaRodada) {
-        // compra do usuário
-        novaCompra = comprarCarta();
-        cartas.push(novaCompra.texto);
-        points += novaCompra.valor
-        // compra da máquina
-        novaCompra = comprarCarta();
-        pointsPc += novaCompra.valor
-
-        if (points !== pointsPc) {
-       //revelação das cartas
-        console.log("Suas cartas são ---->  " + cartas + ". || A carta revelada do computador é " + novaCompra.texto)
-
-        } else {
-         console.error("Cartas iguais. Novo sorteio...")
-         novaRodada = confirm("Novo sorteio?")
-        }
-
-        
-        if (points < 21 || pointsPc < 21) {
-           novaRodada = confirm("Deseja comprar mais uma carta?")
-         } else if (pointsPc > points) {
-            console.error("Você perdeu :(");
-            console.log("O computador fez " + pointsPc + " pontos.")
-            break
-         } else if (points > pointsPc) {
-            console.warn("Você ganhou!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            console.log("Parabéns, fez " + points + " pontos.")
-            break
-         }
-
+            } 
+      // acaba o jogo se o usuário clica em cancelar  
+      } else if(!novaRodada) {
+         console.log("O jogo acabou :(")
       }
-         
-   }
-   
-   // else if (user1.texto == "A" && user2.texto == "A" || pc1.texto == "A" && pc2.texto == "A" ){
-   // //    console.log("Cartas iguais. Novo sorteio...")
-   // //    novaRodada = confirm("Novo sorteio?")
-   // // }
-     
-     
-
-    
-   
-   //  if(!novaRodada) {
-   //     console.log("O jogo acabou :(")
-   //  } else if (novaRodada) {
-   //    console.log("Suas cartas são: " + novaCompra.texto + " | " + novaCompra.texto + " | " + "A carta revelada do computador é: " + pc1.texto + "\n Deseja comprar mais uma carta?")
-   //    if (user1.texto == "A" && user2.texto == "A" || pc1.texto == "A" && pc2.texto == "A" ){
-   //       console.log("Cartas iguais. Novo sorteio...")
-   //       novaRodada = confirm("Novo sorteio?")
-   //    }
-   //    while (novaRodada) {
-   //       novaCompra = comprarCarta();
-   //       cartas.push(novaCompra.texto);
-   //       console.log("Suas cartas são:  " + cartas + "  ")
-   //       points += novaCompra.valor
-   //       if (points < 21) {
-   //          novaRodada = confirm("Deseja comprar mais uma carta?")
-   //       } else {
-   //          console.log("Você ganhou")
-   //          break
-   //       }
-         
-   //    }
-   //    console.log("Parabéns, fez " + points + " pontos.")
-       
-
-   //    }
-
-
-      // let i=0;
-      // while (novaRodada) {
-      //    novaCompra = comprarCarta();
-      //    cartas.push(novaCompra)
-      //    i++
-      // }
-      
-
-    
-
-   //  if(!novaRodada) {
-   //    console.log("O jogo acabou.")
-   //  } else {
-   //    console.log("Suas cartas são: " + user1.texto + " | " + user2.texto + " | " + "A carta revelada do computador é: " + pc1.texto)
-   //    novaRodada = confirm("Deseja comprar mais uma carta?")
-   //    console.log("Cartas da máquina: " + pc1.texto + " | " + pc2.texto + " | " + "Pontuação: " +  res2)
-   //    if (res1 >res2) {
-   //       console.log("Você ganhou!")
-   //    } else if (res1 === res2){
-   //       console.log("Deu empate! Jogue uma nova rodada!")
-   //    } else {
-   //       console.log("Você perdeu :(")
-   //    }
-   //  }
--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -1,18 +1,5 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+
    //  const carta = comprarCarta();
-
-
 
    //  console.log("Bem vindo ao jogo BlackJack!")
    //  let user1 = comprarCarta();;
@@ -39,18 +26,3 @@
    //  }
 
 
-
-
-
-   //  console.log(comprarCarta())
-   //  console.log(comprarCarta())
-   //  console.log(comprarCarta())
-   //  console.log(comprarCarta())
-   //  console.log(comprarCarta())
-
-   //  console.log(user)
-   //  console.log(user)
-   //  console.log(user)
-   //  console.log(pc)
-   //  console.log(pc)
-   //  console.log(pc)
