dd3bb8a6869464f283469969028b4dd685835b6e
Luana <lualuna182@gmail.com>
2021-03-11 15:56:59 -0300

    Manipulação de listas

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,14 +3,76 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    posts: [],
+  
+    inputNome: "",
+    inputFotoUser: "",
+    inputPost: ""
+  };
+
+  onChangeInputNome = (event) => {
+    this.setState({ inputNome: event.target.value })
+  };
+
+  onChangeFotoUser = (event) => {
+    this.setState({ inputFotoUser: event.target.value })
+  };
+
+  onChangeinputPost = (event) => {
+    this.setState({ inputPost: event.target.value })
+  };
+
+  criaPost = () => {
+    const post = {
+      nome: this.state.inputNome,
+      fotoUsuario: this.state.inputFotoUser,
+      post: this.state.inputPost
+    };
+
+    const novoPost = [...this.state.posts, post];
+
+    this.setState({ posts: novoPost, inputNome: "", inputFotoUser: "", inputPost: "" });
+    console.log(novoPost)
+  };
+
+
   render() {
+
+    const listaPosts = this.state.posts.map((item) => {
+      return (
+        <Post
+        nomeUsuario={item.nome}
+        fotoUsuario={item.fotoUsuario}
+        fotoPost={item.post}
+      />
+
+      )
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'lua182'}
-          fotoUsuario={'https://picsum.photos/40/50'}
-          fotoPost={'https://picsum.photos/201/150'}
+        <input 
+        value={this.state.inputNome}
+        placeholder={"Nome"} 
+        onChange={this.onChangeInputNome}
         />
+        <input 
+        value={this.state.inputFotoUser}
+        placeholder={"Foto Usuario"} 
+        onChange={this.onChangeFotoUser}
+        />
+        <input 
+        value={this.state.inputPost}
+        placeholder={"Foto"} 
+        onChange={this.onChangeinputPost}
+        />
+        <button onClick={this.criaPost}>Enviar</button>
+
+
+        {listaPosts} 
+        
+
         <Post
           nomeUsuario={'alesilva'}
           fotoUsuario={'https://picsum.photos/60/50'}
@@ -25,5 +87,6 @@ class App extends React.Component {
     );
   }
 }
+
 
 export default App;
