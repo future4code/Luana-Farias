6105558109ab484bf2ac42ae8df9121dcfc52a53
Luana <lualuna182@gmail.com>
2021-03-15 21:06:16 -0300

    labeform

---
+++ semana6/labeform/src/componentes/FinalFormulario.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default class DadosGerais extends React.Component {
+
+    render () {
+
+        return (
+            <div>
+                <h1>O formulário acabou!</h1>
+                <p>Obrigada por participar! Entraremos em contato.</p>
+
+            </div>
+
+        )
+    }
+}
